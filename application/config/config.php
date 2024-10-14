4c7a74fe4e737464d5db272ec9a7e85ea397036f
Steven Agang <agangsteven19@gmail.com>
2024-10-15 07:07:20 +0800

    make some changes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://urlanalyzer/';
+$config['base_url'] = 'https://htmlanalyzer.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/mains.php
+++ application/controllers/mains.php
@@ -18,6 +18,10 @@ class mains extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
+    public function __construct() {
+        parent::__construct();
+        $this->output->enable_profiler(false);
+    }
 	public function index()
 	{
 		$view_data = $this->base_data();
