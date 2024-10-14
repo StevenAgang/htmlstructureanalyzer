e8946980d927e3f23a83c7e934132df403163c93
Steven Agang <agangsteven19@gmail.com>
2024-10-15 07:17:45 +0800

    changing ajax action to Mains

--- assets/js/main_process.js
+++ assets/js/main_process.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
                 return false;
             }else{
                 $('#modal').css('display','flex');
-                $.get('mains/analyzer',$(this).serialize(),function(res){
+                $.get('Mains/analyzer',$(this).serialize(),function(res){
                     if(res.status === true){
                         $('#tags').html(res.tags);
                         $('#response pre').text('');
