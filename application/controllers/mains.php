9eed9b3942b42858e652c8a6248e5a283f3b7a28
Steven Agang <agangsteven19@gmail.com>
2024-10-15 07:01:14 +0800

    changing mains controller to lowercase

--- application/controllers/mains.php
+++ application/controllers/mains.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Mains extends CI_Controller {
+class mains extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
