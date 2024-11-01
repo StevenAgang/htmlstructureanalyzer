8d5d5fa0b6140b198db883251492675c074369df
Steven Agang <agangsteven19@gmail.com>
2024-11-01 15:59:29 +0800

    adding icon

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -3,9 +3,11 @@
     <head>
         <meta charset="utf-8"/>
         <meta name="viewport"content="width=device-width, initial-scale=1.0"/>
-        <meta name="description" description="HTTP analyzer"/>   
-        <title>HTTP Analyzer</title>
+        <meta name="description" content="HTML Structure Analyzer"/>   
+        <meta name="keywords" content="analyzer, html analyzer, structure analyzer ">
+        <title>HTML Structure Analyzer</title>
         <link rel="stylesheet" href="/assets/css/main_style.css">
+        <link rel="icon" type="image/x-icon" href="/assets/img/icons.jpg"/>
         <script src="/assets/lib/jquery-3.7.1.js"></script>
         <script src="/assets/js/main_process.js"></script>
     </head>
